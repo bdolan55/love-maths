8554f9d24207599efd155af40eae6bc58de4dce5
bdolan55 <brian.e.dolan88@gmail.com>
2023-01-06 18:52:15 +0000

    code added to increment the correct answer score and incorrect answer score on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,8 +48,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey you got it right!!");
+        incrementScore();
     }else {
         alert(`Awwww... you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -74,11 +76,26 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the dom and increments it b y 1
+ */
+
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore; // or can use = oldScore + 1;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the dom and increments it by 1
+ */
+
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+
 
 }
 
