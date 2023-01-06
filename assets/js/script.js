50d9d08619fbac9bee5db2c458841068bf4cb670
bdolan55 <brian.e.dolan88@gmail.com>
2023-01-06 20:31:26 +0000

    minus game code added and working

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,8 +29,10 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1,num2);
     } else if (gameType === "multiply") {
-            displaymultiplyQuestion(num1, num2);
+        displaymultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}.Aborting!`;
@@ -71,6 +73,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else {
@@ -112,6 +116,9 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion() {
 
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 }
 
 function displaymultiplyQuestion(operand1, operand2) {
